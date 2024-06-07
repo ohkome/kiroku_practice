8deb6973b6d3713d3526da6b2480bf75421061f0
ohkome <workskome@gmail.com>
2024-06-07 23:28:34 +0900

    burannti

---
+++ src/main/java/com/practice/kiroku/kiroku_app/KirokuAppApplication.java
@@ -0,0 +1,13 @@
+package com.practice.kiroku.kiroku_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirokuAppApplication.class, args);
+	}
+
+}
